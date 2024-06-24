055206c74b4f340046d9a139af0ccc54eac403b5
Carlos VILLAGOMEZ <carlos_vialfa@hotmail.com>
2024-06-24 09:22:34 -0500

    Update vectorizing.py

--- vectorizing.py
+++ vectorizing.py
@@ -1,9 +1,9 @@
 import numpy as np
 train_data = [[3,5],[1,7,2,8],[5,9,6]]
-def vectorize_sequences(sequences, dimension=10):
+def vectorize_sequences(sequences, dimension=10): # sequences is equal to train_data a sequece of reviews in imdb
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
-        results[i, sequence] = 1
+        results[i, sequence] = 1 # Assignment of Elements in the Matrix
     return results
 x_train = vectorize_sequences(train_data)
 x_train
